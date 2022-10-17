d68fd49c9cd7fbe1f7206cb7969fe4819a877785
youngsung park <yschrisp3759@gmail.com>
2022-10-18 01:52:59 +0900

    회원가입 세팅

---
+++ spaceXnote/src/main/java/com/example/spacexnote/dto/MemberRequestDto.java
@@ -0,0 +1,25 @@
+package com.example.spacexnote.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberRequestDto {
+
+    @NotBlank(message = "{member.nickname.notblank}")
+    @Size(min = 4, max = 12, message = "{member.nickname.size}")
+    @Pattern(regexp = "[a-z\\d]*${3,12}", message = "{member.nickname.pattern}")
+    private String nickname;
+
+    @NotBlank(message = "{member.password.notblank}")
+    @Size(min = 8, max = 20, message = "{member.password.size}")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$"
+            , message = "{member.password.pattern}")
+    private String password;
+
+    @NotBlank
+    private String passwordConfirm;
+}
---
+++ spaceXnote/src/main/java/com/example/spacexnote/service/Memberservice.java
@@ -0,0 +1,29 @@
+package com.example.spacexnote.service;
+
+import com.example.spacexnote.dto.MemberRequestDto;
+import com.example.spacexnote.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Memberservice {
+    private final PasswordEncoder passwordEncoder;
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemeberService(PasswordEncoder passwordEncoder, MemberRepository memberRepository) {
+        this.passwordEncoder = passwordEncoder;
+        this.memberRepository = memberRepository;
+    }
+
+    public void registerUser(MemberRequestDto requestDto) {
+        String membername = requestDto.getMembername();
+        String email = requestDto.getEmail();
+
+        //패스워드 암호화
+        String password = passwordEncoder.encode(requestDto.getPassword());
+
+        User user = new User(nickname, password, email);
+        userRepository.save(user);
+    }
+}
