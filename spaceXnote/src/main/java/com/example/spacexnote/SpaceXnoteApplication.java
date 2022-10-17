78156ad91fbae6534812c6b7b7d5cc6db5a429cb
Jaylin16 <jaylin6115@gmail.com>
2022-10-18 01:12:16 +0900

    Timestamped 추가, comment CUD 구현 중. 22.10.18.

--- spaceXnote/src/main/java/com/example/spacexnote/SpaceXnoteApplication.java
+++ spaceXnote/src/main/java/com/example/spacexnote/SpaceXnoteApplication.java
@@ -2,8 +2,10 @@ package com.example.spacexnote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpaceXnoteApplication {
 
     public static void main(String[] args) {
---
+++ spaceXnote/src/main/java/com/example/spacexnote/service/CommentService.java
@@ -0,0 +1,21 @@
+package com.example.spacexnote.service;
+
+import com.example.spacexnote.repository.CommentRepository;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@NoArgsConstructor
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+    //댓글 생성해주는 메소드.
+
+    //댓글 수정해주는 메소드.
+
+    //댓글 삭제하는 메소드.
+
+    //댓글 전체 리스트 만들어주는 메소드. postId로 매핑해서 게시글 조회 페이지에서 보여주기.
+
+}
