7324a19431901bf150225a189f427369a8a35395
Bang <rlaqudrl831@naver.com>
2022-10-18 11:36:43 +0900

    10. 18 / 11:36

---
+++ spaceXnote/src/main/java/com/example/spacexnote/security/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.example.spacexnote.security;
+
+import com.example.spacexnote.entity.PasswordEncoder;
+import com.example.spacexnote.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
+import org.springframework.boot.autoconfigure.security.servlet.AntPathRequestMatcherProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    MemberService memberService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .loginPage("/members/login")
+                .defaultSuccessUrl("/")
+                .usernameParameter("email")
+                .failureUrl("/members/login/error")
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcherProvider("/members/logout"))
+
+                .logoutSuccessUrl("/");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(memberService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+}
---
+++ spaceXnote/src/main/java/com/example/spacexnote/service/UserDetailsService.java
@@ -0,0 +1,4 @@
+package com.example.spacexnote.service;
+
+public interface UserDetailsService {
+}
