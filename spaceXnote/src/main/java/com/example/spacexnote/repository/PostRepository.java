8816f0f6f6b5850a46176056f87db1e219fb7d83
Seo Dongjun <ilkasd616@gmail.com>
2022-10-19 22:06:19 +0900

    Post, Comment 수정 완료

--- spaceXnote/src/main/java/com/example/spacexnote/repository/PostRepository.java
+++ spaceXnote/src/main/java/com/example/spacexnote/repository/PostRepository.java
@@ -1,7 +1,7 @@
 package com.example.spacexnote.repository;
 
 
-import com.example.demo.entity.Post;
+import com.example.spacexnote.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
