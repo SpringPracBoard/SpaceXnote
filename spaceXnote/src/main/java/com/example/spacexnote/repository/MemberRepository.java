82ad71532b363b7cbd1f0796a81aad9e766d5589
Seo Dongjun <ilkasd616@gmail.com>
2022-10-19 23:44:58 +0900

    no message

--- spaceXnote/src/main/java/com/example/spacexnote/repository/MemberRepository.java
+++ spaceXnote/src/main/java/com/example/spacexnote/repository/MemberRepository.java
@@ -1,19 +1,4 @@
-<<<<<<< HEAD
-//package com.example.spacexnote.repository;
-//
-//import com.example.spacexnote.entity.Member;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//import java.util.Optional;
-//
-//@Repository
-//public interface MemberRepository extends JpaRepository<Member, Long> {
-//    Optional<Member> findByMembername(String membername);
-//    boolean existsByEmail(String email);
-//    boolean existsMemberBy(String nickname);
-//
-//}
-=======
+
 package com.example.spacexnote.repository;
 
 import com.example.spacexnote.entity.Member;
@@ -26,4 +11,3 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
 
 }
->>>>>>> f8be561e8d3c233d00f68724ca14a5f9da0f3373
